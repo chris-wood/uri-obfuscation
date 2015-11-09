4b228745317224413fa1f9914f94d3471534eb27
Christopher Wood <christopherwood07@gmail.com>
2015-11-08 16:38:31 -0800

    Distribution code

--- distribution.py
+++ distribution.py
@@ -1,10 +1,15 @@
 import os
 import sys
 
-# TODO: build up the map
+def compute_pmf(pairs):
+    pmf = {}
+    for pair in pairs:
+        pairin = pair[0]
+        pairout = pair[1]
+        key = (pairin, pairout)
+        pmf[key] = 0 if key not in pmf else pmf[key] + 1
+    return pmf
 
 with open(sys.argv[1], "r") as f:
     pairs = map(lambda line: line.strip().split(","), f.readlines())
-    for pair in pairs:
-        namein = pair[0]
-        nameout = pair[1]
+    compute_distribution(pairs)
