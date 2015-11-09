af13d593d3dd32866680dd64a3e679301d1b708f
Christopher Wood <christopherwood07@gmail.com>
2015-11-08 16:38:38 -0800

    Fixed the obfuscator

--- obfuscate.py
+++ obfuscate.py
@@ -20,14 +20,14 @@ class Obfuscator(object):
 
     def encrypt_cbc(self, blob):
         global backend
-        cipher = Cipher(algorithms.AES(self, key), modes.CBC(self.iv), backend=backend)
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=backend)
         encryptor = cipher.encryptor()
         ct = encryptor.update(blob) + encryptor.finalize()
         return self.iv, ct, None
 
     def encrypt_cbc(self, blob):
         global backend
-        cipher = Cipher(algorithms.AES(self, key), modes.GCM(self.iv), backend=backend)
+        cipher = Cipher(algorithms.AES(self.key), modes.GCM(self.iv), backend=backend)
         encryptor = cipher.encryptor()
         ct = encryptor.update(blob) + encryptor.finalize()
         return self.iv, ct, encryptor.tag
@@ -47,11 +47,11 @@ class URI(object):
 
     def obfuscate_encrypt_structured(self, index):
         ouri = self.uri[:index]
-        for i in range(index, len(self.uri) + 1):
+        for i in range(index, len(self.uri)):
             plaintext = self.uri[i]
             iv, ct, tag = self.obfuscator.encrypt_cbc(plaintext)
             ouri.append(ct)
-        return self.ouri
+        return ouri
 
     def obfuscate_encrypt_blob(self, index):
         plaintext = "/".join(self.uri[index:])
@@ -59,7 +59,15 @@ class URI(object):
         ouri = self.uri[:index] + [ct]
         return ouri
 
-    def obfuscate_hash(self, index):
+    def obfuscate_hash_structured(self, index):
+        ouri = self.uri[:index]
+        for i in range(index, len(self.uri)):
+            plaintext = self.uri[i]
+            ct = self.obfuscator.hash_hmac(plaintext)
+            ouri.append(ct)
+        return ouri
+
+    def obfuscate_hash_blob(self, index):
         plaintext = "/".join(self.uri[index:])
         ct = self.obfuscator.hash_hmac(plaintext)
         ouri = self.uri[:index] + [ct]
@@ -72,23 +80,30 @@ def main(args):
     if len(args) == 1:
         obfuscator = Obfuscator()
         uri = URI("/a/b/c", obfuscator)
-        ouri = uri.obfuscate_encrypt_blob(1)
-
         print uri
+
+        ouri = uri.obfuscate_encrypt_blob(1)
         print ouri
 
+        ouri = uri.obfuscate_encrypt_blob(2)
+        print ouri
+
+        ouri = uri.obfuscate_encrypt_blob(3)
+        print ouri
     elif args > 1:
         obfuscator = Obfuscator()
         with open(args[1], "r") as fhandle:
-            line = fhandle.readline().strip()
-            uri = URI(line, obfuscator)
+            for line in fhandle:
+                line = line.strip()
+                uri = URI(line, obfuscator)
 
-            for i in range(uri.number_of_components()):
-                ouri = uri.obfuscate_encrypt_blob(i)
-                ouri = uri.obfuscate_encrypt_structured(i)
-                ouri = uri.obfuscate_hash(i)
+                for i in range(uri.number_of_components()):
+                    ouri1 = uri.obfuscate_encrypt_blob(i)
+                    ouri2 = uri.obfuscate_encrypt_structured(i)
+                    ouri3 = uri.obfuscate_hash_blob(i)
+                    ouri4 = uri.obfuscate_hash_structured(i)
 
-                # TODO: do something with these hashes... combine them somehow?
+                    print i, uri, ouri1, ouri2, ouri3, ouri4
 
 if __name__ == "__main__":
     main(sys.argv)
