b74f345162b48ebd70e2c68a2ede4f10ce5b9404
Christopher Wood <christopherwood07@gmail.com>
2015-11-12 14:04:10 -0800

    joint PMF

--- distribution.py
+++ distribution.py
@@ -26,18 +26,33 @@ def compute_entropy_efficiency(pairs):
     # n = number of bits in each element of the alphabet
     pass
 
-def compute_joint_pmf(pairs_list):
-    if len(pairs_list) == 1:
-        return compute_pmf(pairs_list[0])
-    elif len(pairs_list) == 2:
-        # p(x,y) = p(X = x ^ Y = y)
-        # X = pairs[0], Y = pairs[1]
-        pass
-    else: # more than one DRV
-        pass
+def compute_joint_pmf(samples_list):
+    '''
+    Input: list of samples lists: [[sample-list-1], [sample-list-2], ...]
+    Output: map from (symbol1, symbol2, ...) -> probability
+    ''' 
+    pmf = {}
+    
+    num_samples = reduce(lambda x, y: min(len(x), len(y)), samples_list)
+    num_lists = len(samples_list)
+    for i in range(num_samples):
+        symbols = []
+        for j in range(num_lists):
+            symbols.append(samples_list[j][i])
+
+        key = tuple(symbols)
+        pmf[key] = 1 if key not in pmf else pmf[key] + 1
+
+    total = num_samples
+    for key in pmf:
+        pmf[key] = pmf[key] / float(total)
+
+    return pmf
 
 def compute_pmf(samples):
-    ''' Pairs is a list of samples
+    ''' 
+    Input: list of samples
+    Output: map from symbol -> probability
     ''' 
     pmf = {}
     for sample in samples:
@@ -54,6 +69,8 @@ def compute_distribution(pairs):
     for i in range(num_cols):
         pmf = compute_pmf(pairs[i])
         print pmf
+    pmf = compute_joint_pmf(pairs)
+    print pmf
 
 with open(sys.argv[1], "r") as f:
     matrix = map(lambda line: line.strip().split(","), f.readlines())
@@ -62,3 +79,4 @@ with open(sys.argv[1], "r") as f:
     for i in range(length):
         pairs.append(map(lambda l : l[i], matrix))
     compute_distribution(pairs)
+
