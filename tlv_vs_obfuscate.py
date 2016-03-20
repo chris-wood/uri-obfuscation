93401792e8ef71619a2f5eb55eecb1b04daf24d6
Cesar Ghali <cesarghali.p@gmail.com>
2016-03-19 18:09:52 -0700

    Free large variables after plotting PDF

--- tlv_vs_obfuscate.py
+++ tlv_vs_obfuscate.py
@@ -221,42 +221,49 @@ def plotResultsFromFile(inputPath, verbose, errorType):
         with open(os.path.join(inputPath, "tlv.out"), "r") as inFile:
             tmp = pickle.load(inFile)
         plot(tmp, "TLV format")
+        del tmp
         print("--- %s seconds ---" % (time.time() - start_time))
 
         print "\t" + os.path.join(inputPath, "hash16.out") + "..."
         with open(os.path.join(inputPath, "hash16.out"), "r") as inFile:
             tmp = pickle.load(inFile)
         plot(tmp, "16-bit format")
+        del tmp
         print("--- %s seconds ---" % (time.time() - start_time))
 
         print "\t" + os.path.join(inputPath, "hash32.out") + "..."
         with open(os.path.join(inputPath, "hash32.out"), "r") as inFile:
             tmp = pickle.load(inFile)
         plot(tmp, "32-bit format")
+        del tmp
         print("--- %s seconds ---" % (time.time() - start_time))
 
         print "\t" + os.path.join(inputPath, "hash48.out") + "..."
         with open(os.path.join(inputPath, "hash48.out"), "r") as inFile:
             tmp = pickle.load(inFile)
         plot(tmp, "48-bit format")
+        del tmp
         print("--- %s seconds ---" % (time.time() - start_time))
 
         print "\t" + os.path.join(inputPath, "hash64.out") + "..."
         with open(os.path.join(inputPath, "hash64.out"), "r") as inFile:
             tmp = pickle.load(inFile)
         plot(tmp, "64-bit format")
+        del tmp
         print("--- %s seconds ---" % (time.time() - start_time))
 
         print "\t" + os.path.join(inputPath, "hash128.out") + "..."
         with open(os.path.join(inputPath, "hash128.out"), "r") as inFile:
             tmp = pickle.load(inFile)
         plot(tmp, "128-bit format")
+        del tmp
         print("--- %s seconds ---" % (time.time() - start_time))
 
         print "\t" + os.path.join(inputPath, "hash160.out") + "..."
         with open(os.path.join(inputPath, "hash160.out"), "r") as inFile:
             tmp = pickle.load(inFile)
         plot(tmp, "160-bit format")
+        del tmp
         print("--- %s seconds ---" % (time.time() - start_time))
 
         finalizePlotPDF()
@@ -313,6 +320,7 @@ def finalizePlotPDF():
     plt.grid(True)
     plt.xlabel("Name length (bytes)")
     plt.ylabel("Probability density function")
+    plt.xlim([0, 500])
     plt.legend()
 
     # Save to file.
