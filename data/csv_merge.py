081d64871ceaabaaea60b9594bbd695aa65cfac5
Christopher Wood <christopherwood07@gmail.com>
2016-03-24 06:34:32 -0700

    Multi entropy calculation updates

--- data/csv_merge.py
+++ data/csv_merge.py
@@ -2,6 +2,7 @@ import os
 import sys
 import csv
 
+min_key = 100000 # something big
 with open(sys.argv[1], 'r') as infh:
     fnames = infh.readlines()
     names = []
@@ -15,15 +16,27 @@ with open(sys.argv[1], 'r') as infh:
                 key = int(line[0])
                 if key not in data:
                     data[key] = []
-                data[key].append(line[1])
+                data[key].append(float("{0:.3f}".format(float(line[1]))))
 
+                if key < min_key:
+                    min_key = key
 print names
 print data
 
-with open(sys.argv[1] + ".csv", 'wb') as csvfile:
+with open(sys.argv[1] + ".joint.csv", 'wb') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
     names = [""] + names
     writer.writerow(names)
     for key in data:
         line = [key] + data[key]
+        writer.writerow(line)
+
+with open(sys.argv[1] + ".cond.csv", 'wb') as csvfile:
+    writer = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+    names = [""] + names
+    writer.writerow(names)
+    for key in data:
+        if key == min_key:
+            continue
+        line = [key] + map(lambda v : float("{0:.3f}".format(v)), map(lambda (f, p): f - p, zip(data[key], data[key - 1])))
         writer.writerow(line)
--- multi.py
+++ multi.py
@@ -8,12 +8,9 @@ import numpy as np
 NUM_CORES = 8
 
 def array_to_key(a, n):
-    symbols = []
-    for i in range(n):
-        symbols.append(a[i])
-    return tuple(symbols)
+    return tuple(a[0:n])
 
-def group_counts(result, pool):
+def group_counts(result):
     groups = {}
     total = 0
     for item in result:
@@ -21,60 +18,69 @@ def group_counts(result, pool):
         total += 1
     return groups, total
 
-def reduce(l):
-    return (l[0], sum(pair[1] for pair in l[1]))
-
 def entropy_product(p):
     return p * np.log2(p) if p > 0 else 0
 
-def compute_entropy(rows, cmax):
-    # with ProcessPoolExecutor() as executor:
+def parallel_entropy(matrix, cmax):
+    rows = []
+    for row in matrix:
+        if len(row) >= cmax:
+            rows.append(row)
     with ThreadPoolExecutor(NUM_CORES) as executor:
-        print >> sys.stderr, "Create keys..."
         keys = list(executor.map(lambda r : array_to_key(r, cmax), rows))
-        print >> sys.stderr, "Done."
-
-        print >> sys.stderr, "Group counts..."
-        group, count = group_counts(keys, None)  #executor.submit(lambda l : group_counts(l, executor), result)
-        print >> sys.stderr, "Done"
-
-        print >> sys.stderr, "Compute JPMF..."
+        group, count = group_counts(keys)
         jpmf = dict(executor.map(lambda l : (l, float(group[l]) / count), group))
-        print >> sys.stderr, "Done"
-
-        print >> sys.stderr, "Compute entropy..."
         entropy = sum(list(executor.map(lambda key : entropy_product(jpmf[key]), jpmf))) * -1
-        print >> sys.stderr, "Done."
+        return (cmax, entropy)
 
-        return entropy
+def sequential_entropy(matrix, cmax):
+    rows = []
+    for row in matrix:
+        if len(row) >= cmax:
+            rows.append(row)
+
+    keys = list(map(lambda r : array_to_key(r, cmax), rows))
+    group, count = group_counts(keys)
+    jpmf = dict(map(lambda l : (l, float(group[l]) / count), group))
+    entropy = sum(map(lambda key : entropy_product(jpmf[key]), jpmf)) * -1
+    return (cmax, entropy)
 
 # args[1] = input filename
 # args[2] = max number of columns
-# args[3] = output filename
-def main(args):
+def parallel(args):
     with open(args[1], "r") as f:
         matrix = map(lambda line: line.strip().split("/"), f.readlines())
-        num_cols = int(args[2]) # max number of components in a URI
+        num_cols = int(args[2])
         num_rows = len(matrix)
 
-        print >> sys.stderr, "Pulled the matrix into memory."
+        start = time.time()
+        entropy_results = []
+        with ThreadPoolExecutor(NUM_CORES) as executor:
+            entropy_results = executor.map(lambda cols : parallel_entropy(matrix, cols), range(1, num_cols + 1))
+        end = time.time()
 
-        for cmax in range(1, num_cols + 1):
-            rows = []
-            for row in matrix:
-                if len(row) >= cmax:
-                    rows.append(row)
+        for (cmax, entropy) in entropy_results:
+            print cmax, entropy
 
-            print >> sys.stderr, "Starting computation for CMAX=%d" % (cmax)
-            start = time.time()    
-            entropy = compute_entropy(rows, cmax)
-            end = time.time()
-            print >> sys.stderr, "Elapsed time: %s" % (str(end - start))
-            print entropy
+        print "Total time: %f" % (end - start)
 
-            with open(sys.argv[3], "a") as fout:
-                writer = csv.writer(fout, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-                writer.writerow([cmax, entropy])
+def sequential(args):
+    with open(args[1], "r") as f:
+        matrix = map(lambda line: line.strip().split("/"), f.readlines())
+        num_cols = int(args[2])
+        num_rows = len(matrix)
+
+        start = time.time()
+        entropy_results = map(lambda cols : sequential_entropy(matrix, cols), range(1, num_cols + 1))
+        end = time.time()
+
+        for (cmax, entropy) in entropy_results:
+            print cmax, entropy
+
+        print "Total time: %f" % (end - start)
 
 if __name__ == "__main__":
-    main(sys.argv)
+    if sys.argv[1] == "-p":
+        parallel(sys.argv[1:])
+    else:
+        sequential(sys.argv[1:])
