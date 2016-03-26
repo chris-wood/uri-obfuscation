fc7721a439a2bad0d7eb0715ddf1524feaaef5b9
Christopher Wood <christopherwood07@gmail.com>
2016-03-26 13:35:45 -0700

    Proper iteration.

--- cisco/compute_all_entropy.sh
+++ cisco/compute_all_entropy.sh
@@ -9,28 +9,33 @@ do
     index=1
     while [ $index -lt $MAX_INDEX ]
     do
-        OUT=${URI_FILE}_${ALG}_${index}_flatten.txt
-        DATA_OUT=${OUT}.csv
-        cmd="python ../parallel_entropy.py ${OUT} ${index} > ${DATA_OUT}"
-        echo $cmd
-        eval $cmd
+        offset=1
+        while [ $offset -lt $MAX_INDEX ]
+        do
+            OUT=${URI_FILE}_${ALG}_${index}_flatten.txt
+            DATA_OUT=${URI_FILE}_${ALG}_${index}_${offset}_flatten.csv
+            cmd="python ../parallel_entropy.py ${OUT} ${offset} > ${DATA_OUT}"
+            echo $cmd
+            eval $cmd
 
-        if [ $? -ne 0 ]; then
-            echo "FAILURE on ${OUT}"
-            exit
-        fi
+            if [ $? -ne 0 ]; then
+                echo "FAILURE on ${OUT}"
+                exit
+            fi
 
-        OUT=${URI_FILE}_${ALG}_${index}.txt
-        DATA_OUT=${OUT}.csv
-        cmd="python ../parallel_entropy.py ${OUT} ${index} > ${DATA_OUT}"
-        echo $cmd
-        eval $cmd
+            OUT=${URI_FILE}_${ALG}_${index}.txt
+            DATA_OUT=${URI_FILE}_${ALG}_${index}_${offset}.csv
+            cmd="python ../parallel_entropy.py ${OUT} ${offset} > ${DATA_OUT}"
+            echo $cmd
+            eval $cmd
 
-        if [ $? -ne 0 ]; then
-            echo "FAILURE on ${OUT}"
-            exit
-        fi
+            if [ $? -ne 0 ]; then
+                echo "FAILURE on ${OUT}"
+                exit
+            fi
 
+            offset=$[$offset+1]
+        done
         index=$[$index+1]
     done
 done
